7f42afeec6cf72003e144c4c02320e9375d9a5e9
Bucinell, Ryan <rbucinell@paychex.com>
2018-11-09 11:44:31 -0500

    created a spawn function to initialize settings, reduced work in update

--- src/js/Bullet.js
+++ src/js/Bullet.js
@@ -9,9 +9,24 @@ export default class Bullet
         this.spd = 5;
     }
 
+    /**
+     * Initializes the bullet's initial configuratoin 
+     * 
+     * @param {Vector2D} position The initial vector location of the bullet
+     * @param {Vector2D} direction The initial unit vector direction of the bullet
+     * @param {Number} speed The velocity of the bullet
+     * @memberof Bullet
+     */
+    spawn( position, direction, speed )
+    {
+        this.pos = position;
+        this.spd = speed;
+        this.dir = Vector2D.multiply( Vector2D.normal(direction), new Vector2D( this.spd, this.spd ));
+    }
+
     update()
     {
-        this.pos = Vector2D.add( this.pos, Vector2D.multiply(this.dir,new Vector2D(this.spd,this.spd)));
+        this.pos = Vector2D.add( this.pos, this.dir );
     }
 
     draw( ctx ){
