285bcc6708a6c3316401bbb1326c7e7ae66c94be
Bucinell, Ryan <rbucinell@paychex.com>
2018-11-14 22:22:58 -0500

    Added direction property to all entities

--- src/js/Entity.js
+++ src/js/Entity.js
@@ -5,6 +5,7 @@ export default class Entity
     constructor() 
     {
         this.pos = new Vector2D(0,0);
+        this.dir = new Vector2D(0,0);
         this.w = 0; //width
         this.h = 0; //height
         this.range = 0; //range
