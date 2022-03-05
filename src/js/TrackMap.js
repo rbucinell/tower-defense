c00f0eda4e71aae32694494aaae967db235f7e55
Ryan Bucinell <rbucinell.dev@gmail.com>
2022-03-04 21:58:36 -0500

    cleaned up tile loading and rendering

--- src/js/TrackMap.js
+++ src/js/TrackMap.js
@@ -1,6 +1,6 @@
 import Atlas from './Atlas.js'
-
-var DEBUG_MODE = true;
+import Sprite from './ui/Sprite.js';
+import Tile from './ui/Tile.js';
 
 export default class TrackMap
 {
@@ -16,67 +16,33 @@ export default class TrackMap
         this.MapTileHeight = json.map_tile_height;
 
         this.offset = offset;
-        this.spawn = spawn;
-        
+        this.spawner = spawn;
+        this.spawnerSprite = new Sprite( this.spawner.x - this.TileWidth/2, this.spawner.y, this.TileWidth, this.TileHeight, this.Atlas, "mapTile_114.png" ); 
         this.width = this.TileWidth * this.MapTileWidth;
         this.height = this.TileHeight * this.MapTileHeight;
         
         this.BaseColor = json.base_color;
-        this.BackgroundImage = json.background_img;
+        this.BackgroundImage = json.background_img;       
         
-        this.TerrainTiles = [];
-        this.PathTiles = [];
-        this.Path = [];
-        this.ObjectTiles = [];
-
+        this.tiles = {
+            terrain: [],
+            path: [],
+            objects: [],
+            towers: []
+        };
     }
 
-    LoadTiles()
+    loadTiles()
     {
-        function populateList( list, jsonArr, tWidth, tHeight )
-        {
-            for( var t in jsonArr )
-            {
-                var tile = jsonArr[t];
-                tile.x = tile.x * tWidth;
-                tile.y = tile.y * tHeight;
-                list.push( jsonArr[t] );
-            }
-        }        
-        populateList( this.TerrainTiles,     this.json.terrain,      this.TileWidth, this.TileHeight );
-        populateList( this.ObjectTiles,     this.json.objects,      this.TileWidth, this.TileHeight );        
-        populateList( this.PathTiles,         this.json.path.tiles, this.TileWidth, this.TileHeight );
+        const createTiles = ( arr, atlas, w, h, offset ) => 
+            arr.map( jd => 
+                new Tile( jd.x * w +  offset.x, jd.y * h + offset.y, w, h, atlas, jd.texture_name)
+        );
 
-        const fp = this.PathTiles[0];
-        const edge = {
-            x: fp.x,
-            y: fp.y
-        };
-        
-        if( fp.y === 0)
-        {
-            edge.x = fp.x + this.TileWidth / 2;
-        }else if(fp.y  === this.height - this.TileHeight)
-        {
-            edge.x = fp.x + this.TileWidth / 2;
-            edge.y = this.height;
-        }
-        else if( fp.x === 0 )
-        {
-            edge.y = fp.y + this.TileHeight / 2;
-        }
-        else if( fp.x === this.width - this.TileWidth )
-        {
-            edge.x = fp.x + this.TileWidth /2;
-            edge.y = this.height;
-        }
-        this.Path.push(edge);
-        for( let i = 0; i < this.PathTiles.length; i++)
-        {
-            this.Path.push( { 
-                x: this.PathTiles[i].x + this.TileWidth/2, 
-                y: this.PathTiles[i].y + this.TileHeight/2
-            });
+        this.tiles = {
+            terrain: createTiles( this.json.terrain, this.Atlas, this.TileWidth, this.TileHeight, this.offset),
+            path:    createTiles( this.json.path.tiles, this.Atlas, this.TileWidth, this.TileHeight, this.offset),
+            objects: createTiles( this.json.objects, this.Atlas, this.TileWidth, this.TileHeight, this.offset),
         }
     }
 
@@ -90,83 +56,20 @@ export default class TrackMap
         ctx.fillRect(0, 0, this.width, this.height);
         
         //console.log( "Textures for atlas have not loaded!");
-        if( this.Atlas.Textures.length === 0 ) return; 
+        if( this.Atlas.Textures.length === 0 ) return;
 
-        //Render the Terrain
-        const tileCount = this.TerrainTiles.length;
-
-        for( let i = 0; i < tileCount; i++)
-        {
-            var curTerrain = this.TerrainTiles[i];
-            let textureName = curTerrain["texture_name"];
-            //Draw the terrain 
-            this.Atlas.drawTexture( textureName, ctx, 
-                curTerrain.x + this.offset.x, curTerrain.y+ this.offset.y, this.TileWidth, this.TileHeight);
-            
-            //Debug tile border and tile name
-            if (typeof DEBUG_MODE !== 'undefined' && DEBUG_MODE === true) {
-                ctx.strokeStyle = "lightgray";
-                ctx.lineWidth = 1;
-                ctx.strokeRect(curTerrain.x, curTerrain.y, this.TileWidth, this.TileHeight);
-                
-                ctx.fillStyle = "black";
-                ctx.font = "8px Arial";
-                const tileNumber = textureName.substring( 8, textureName.length-4);
-                ctx.fillText(tileNumber ,curTerrain.x, curTerrain.y+8); //y+8 is y+fontsize
-            }
-        }
+        this.tiles.terrain.forEach( tile => tile.draw(ctx));
+        this.tiles.path.forEach( tile => tile.draw(ctx));
         
-        /*
-        //Render the path
-        const settings = {
-            lineWidth: ctx.lineWidth,
-            lineDash: ctx.getLineDash(),
-            strokeStyle: ctx.strokeStyle
-        }        
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = 'gold';
-        ctx.setLineDash([
-            10,
-            1
-        ]);
-        
-        ctx.beginPath();
-        ctx.moveTo( this.Path[0].x, this.Path[0].y);
-        for( let i = 1; i < this.Path.length; i++)
+        for( let tileName in this.tiles )
         {
-            const prev = this.Path[i-1];
-            const cur = this.Path[i];
-            ctx.moveTo(prev.x, prev.y);
-            ctx.lineTo(cur.x, cur.y);
-        }
-        ctx.stroke();
-
-        ctx.lineWidth = settings.lineWidth;
-        ctx.setLineDash( settings.lineDash );
-        ctx.strokeStyle = settings.strokeStyle;
-        */
-        
-        //Draw using the Map tiles
-        const pathCount = this.PathTiles.length;
-        for( let i = 0; i < pathCount; i++ )
-        {
-            const curPath = this.PathTiles[i];
-            this.Atlas.drawTexture( curPath.texture_name, ctx, 
-                curPath.x+ this.offset.x, curPath.y+ this.offset.y, this.TileWidth, this.TileHeight);
+            this.tiles[tileName].forEach( tile => tile.draw(ctx) );
         }
     }
 
     drawForeground( ctx )
     {
-        this.Atlas.drawTexture( "mapTile_114.png", ctx, this.spawn.x - this.TileWidth/2, this.spawn.y, this.TileWidth, this.TileHeight);
-        
-        //Render the objects
-        const objCount = this.ObjectTiles.length;
-        for( let i = 0; i < objCount; i++ )
-        {
-            const curObj = this.ObjectTiles[i];
-            this.Atlas.drawTexture( curObj.texture_name, ctx, curObj.x+ this.offset.x, curObj.y+ this.offset.y, this.TileWidth, this.TileHeight);
-        }
-        
+        this.tiles.objects?.forEach( tile => tile.draw(ctx));
+        this.spawnerSprite.draw( ctx );
     }
 }
